dee9f9924576f44d1cc1793ee4d6327870a8c91a
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2023-08-28 07:48:53 +0200

    fix entrypoints example

--- entrypoints/app.py
+++ entrypoints/app.py
@@ -1,3 +1,4 @@
+from enum import StrEnum
 from math import pi
 from random import randint
 
@@ -5,7 +6,18 @@ from pytmlib import AbstractExercise
 from pytmlib import entrypoint
 
 
+class Circle(StrEnum):
+    DIAMETER = 'diameter'
+    CIRCUMFERENCE = 'circumference'
+    AREA = 'area'
+
+
 class Exercise(AbstractExercise):
+    _operations = {
+        Circle.DIAMETER.value: lambda r: r * 2,
+        Circle.CIRCUMFERENCE.value: lambda r: r * 2 * pi,
+        Circle.AREA.value: lambda r: r * r * pi
+    }
     _epsilon = 0.01
     _hint = r'''
     $$d = 2r$$
@@ -32,9 +44,13 @@ class Exercise(AbstractExercise):
         radius = randint(1, 100)
 
         return self.output \
-            .add_latex('Calculate the diameter of a circle with radius %f.' % radius) \
+            .add_latex(r'''
+            Calculate the diameter of a circle with radius %f.
+            ''' % radius) \
             .add_latex(self._hint) \
-            .add_number_field(name='diameter', label='Diameter', step=self._epsilon) \
+            .add_number_field(name=Circle.DIAMETER,
+                              label='Diameter',
+                              step=self._epsilon) \
             .add_action('Solve', self.solve, radius=radius)
 
     @entrypoint
@@ -42,9 +58,13 @@ class Exercise(AbstractExercise):
         radius = randint(1, 100)
 
         return self.output \
-            .add_latex('Calculate the circumference of a circle with radius %f.' % radius) \
+            .add_latex(r'''
+            Calculate the circumference of a circle with radius %f.
+            ''' % radius) \
             .add_latex(self._hint) \
-            .add_number_field(name='circumference', label='Circumference', step=self._epsilon) \
+            .add_number_field(name=Circle.CIRCUMFERENCE,
+                              label='Circumference',
+                              step=self._epsilon) \
             .add_action('Solve', self.solve, radius=radius)
 
     @entrypoint
@@ -52,9 +72,13 @@ class Exercise(AbstractExercise):
         radius = randint(1, 100)
 
         return self.output \
-            .add_latex('Calculate the area of a circle with radius %f.' % radius) \
+            .add_latex(r'''
+            Calculate the area of a circle with radius %f.
+            ''' % radius) \
             .add_latex(self._hint) \
-            .add_number_field(name='area', label='Area', step=self._epsilon) \
+            .add_number_field(name=Circle.AREA,
+                              label='Area',
+                              step=self._epsilon) \
             .add_action('Solve', self.solve, radius=radius)
 
     def solve(self, **kwargs):
@@ -70,14 +94,15 @@ class Exercise(AbstractExercise):
             .add_action('Back', method)
 
     @classmethod
-    def check(cls, radius, diameter=None, circumference=None, area=None):
-        if diameter is not None:
-            return radius * 2 == diameter
-        if circumference is not None:
-            return abs(radius * 2 * pi - circumference) < cls._epsilon
-        if area is not None:
-            return abs(radius * radius * pi - area) < cls._epsilon
-        return False
+    def check(cls, radius, **kwargs):
+        attribute, value = kwargs.popitem()
+
+        if attribute not in cls._operations:
+            return False
+
+        operation = cls._operations.get(attribute)
+
+        return abs(operation(radius) - value) < cls._epsilon
 
 
 app = Exercise()
